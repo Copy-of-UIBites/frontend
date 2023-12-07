fe05dd255462c75db39d6f15f9fe7ef42fb70b28
Catherine Angel <catheriner40540@gmail.com>
2023-12-07 12:37:13 +0700

    add auth buttons

--- src/components/elements/Core/Navbar.tsx
+++ src/components/elements/Core/Navbar.tsx
@@ -2,10 +2,12 @@
 import React from 'react'
 import { LogoutButton } from './LogoutButton'
 import { useUserProfile } from '@contexts'
-import { Typography } from '@mui/material'
+import { Button, Typography } from '@mui/material'
+import { useRouter } from 'next/router'
 
 export const Navbar = () => {
   const { user } = useUserProfile()
+  const router = useRouter()
   return (
     <div className="bg-white fixed h-16 w-full flex flex-row items-center justify-between p-4 shadow-md">
       <Typography
@@ -14,7 +16,28 @@ export const Navbar = () => {
       >
         UIBites
       </Typography>
-      {!!user && (
+      {!!user ? (
+        <div className="flex flex-row gap-4 items-center">
+          <Button
+            variant="outlined"
+            color="secondary"
+            onClick={() => {
+              router.push('/register')
+            }}
+          >
+            Register
+          </Button>
+          <Button
+            variant="outlined"
+            color="primary"
+            onClick={() => {
+              router.push('/login')
+            }}
+          >
+            Login
+          </Button>
+        </div>
+      ) : (
         <div className="flex justify-end">
           <LogoutButton />
         </div>
