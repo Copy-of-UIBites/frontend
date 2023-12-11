649b21661d05156895d82e485ba3f26982a8c1d3
Natania Deandra <nataniadeandra@gmail.com>
2023-12-11 13:27:47 +0700

    fix permission

--- src/components/modules/Kantin/PublicKantinProfile.tsx
+++ src/components/modules/Kantin/PublicKantinProfile.tsx
@@ -2,19 +2,35 @@ import { axiosInstance } from '@utils'
 import React, { FC } from 'react'
 import { Kantin } from './type'
 import { KantinDetail } from './KantinDetail'
+import { useUserProfile } from '@contexts'
 
 interface PublicKantinProfileProps {
   id: string
 }
-export const PublicKantinProfile: FC<PublicKantinProfileProps> = async ({
-  id,
-}) => {
-  const res = await axiosInstance.get(`/kantin/${id}`)
-  const data = res.data as Kantin
+
+export const PublicKantinProfile: FC<PublicKantinProfileProps> = ({ id }) => {
+  const [data, setData] = React.useState<Kantin | null>(null);
+  const { user } = useUserProfile();
+  const [addToFavorites, setAddToFavorites] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axiosInstance.get(`/kantin/${id}`);
+        const kantinData = res.data as Kantin;
+        setData(kantinData);
+        setAddToFavorites(user?.role === 'User' ? true : false);
+      } catch (error) {
+        console.error('Error fetching kantin data:', error);
+      }
+    };
+
+    fetchData();
+  }, [id, user]);
 
   return (
     <div>
-      <KantinDetail kantin={data} addToFavorites={true} />
+      {data && <KantinDetail kantin={data} addToFavorites={addToFavorites} />}
     </div>
-  )
-}
+  );
+};
